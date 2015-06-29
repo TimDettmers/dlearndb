e13830bed979def6b64894bf29c2a06b4cb90fc5
Tim Dettmers <tim.dettmers@gmail.com>
2015-06-29 15:46:27 +0200

    Initial.

---
+++ cudanet/__init__.py
@@ -0,0 +1,8 @@
+# ----------------------------------------------------------------------------
+# Copyright 2014 Nervana Systems Inc.  All rights reserved.
+# ----------------------------------------------------------------------------
+from cudanet import *
+from cudanet_random import *
+
+
+
---
+++ cudanet/cudanet_random.py
@@ -0,0 +1,9 @@
+'''
+Created on Jun 27, 2015
+
+@author: tim
+'''
+
+
+def test():
+    print 'test random'
